91cbccdaeff25754a8c316c702f0d8aa6780a8a3
NICASIO <81364837+frank04957@users.noreply.github.com>
2024-12-16 03:18:02 +0700

    Add files via upload

---
+++ djangotutorial/note/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add_note, name='add'),
+    path('<int:note_id>/', views.detail, name='note_detail'),
+]
---
+++ djangotutorial/note/views.py
@@ -0,0 +1,26 @@
+from .models import Note
+from django.shortcuts import render,  get_object_or_404,  redirect
+
+
+def index(request):
+    notes = Note.objects.all()
+    return render(request, 'note/index.html', {'notes': notes})
+
+
+def detail(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    return render(request, 'note/detail.html', {'note': note})
+
+
+def add_note(request):
+    if request.method == "POST":
+        title = request.POST.get('title', '').strip()
+        content = request.POST.get('content', '').strip()
+
+        if title and content:
+            Note.objects.create(title=title, content=content)
+            return redirect('/')
+        
+        return render(request, 'note/add_note.html', {'error': 'Title and Content are required'})
+
+    return render(request, 'note/add_note.html')
